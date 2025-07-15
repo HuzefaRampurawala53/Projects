8573262f02942fa6fb947af69205558c4e9e43f2
Soham <pansaresoham10@gmail.com>
2025-07-16 01:45:18 +0530

    Changes made in randon_password gen, saves each password in a seperate file

--- random_password_generator.py
+++ random_password_generator.py
@@ -5,3 +5,15 @@ password=""
 for a in range(length):
     password+=random.choice(letter)
 print(password)
+while True:
+    save = input("Do you want to save this password?(y/n): ")
+    if save.lower() == "y":
+        with open("passwords.txt","a") as file:
+            file.write(password)
+
+        break
+    elif save.lower() == "n":
+        break
+    else:
+        print("Invalid Input")
+        continue
